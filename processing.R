fe176b34c8526c08e5068b8815ad71ebe81ff899
fpirotti <fpirotti@gmail.com>
2020-07-08 10:52:30 +0200

    Created Hex grid

--- processing.R
+++ processing.R
@@ -1,7 +1,5 @@
 library(raster)
-library(sf)
-library(ggplot2)
-library(geosphere)
+library(sf) 
 library(sp)
 wfolder<-"/archivio/shared/geodati/vettoriali/vaia/Cerrai_WRF_grid/"
 # 
@@ -14,8 +12,13 @@ nodes.coords <-  as.data.frame(st_coordinates( nodes.projected))
 names(nodes.coords)<-c("x", "y")
 coordinates(nodes.coords) <-  ~x+y
 
-nodes.hex<-sp::HexPoints2SpatialPolygons(nodes.coords, 500)
+nodes.hex<-sp::HexPoints2SpatialPolygons(nodes.coords, 1000)
+saveRDS(nodes.hex, "nodes.hex.projected")
+nodes.hex.projected<-readRDS("nodes.hex.projected")
+nodes.hex.latlng <-  st_as_sf(nodes.hex.projected)  %>% st_set_crs(3034)   %>% st_transform(4326)
+saveRDS(nodes.hex.latlng, "nodes.hex.latlng")
+nodes.hex.latlng<-readRDS("nodes.hex.latlng")
 
-
+st_write(nodes.hex.latlng, "nodes.hex.latlng.shp", append=F)
 
  
