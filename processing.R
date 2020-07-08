7b577b5ac9ab69b4e345dae33c01d7d39bca8e17
fpirotti <fpirotti@gmail.com>
2020-07-08 10:28:22 +0200

    Hex polygons

--- processing.R
+++ processing.R
@@ -2,9 +2,20 @@ library(raster)
 library(sf)
 library(ggplot2)
 library(geosphere)
+library(sp)
 wfolder<-"/archivio/shared/geodati/vettoriali/vaia/Cerrai_WRF_grid/"
-
+# 
 
 nodes <- readRDS("nodes.RDS")
 
-hexbin:::hexcoords(,)
+nodes.projected <-  nodes  %>% st_set_crs(4326)   %>% st_transform(3034)
+
+nodes.coords <-  as.data.frame(st_coordinates( nodes.projected))
+names(nodes.coords)<-c("x", "y")
+coordinates(nodes.coords) <-  ~x+y
+
+nodes.hex<-sp::HexPoints2SpatialPolygons(nodes.coords, 500)
+
+
+
+ 
