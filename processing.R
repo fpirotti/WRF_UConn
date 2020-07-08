58359207bdd58ace2fee83518d815496373221e8
fpirotti <fpirotti@gmail.com>
2020-07-08 09:37:19 +0200

    Initial commit

---
+++ processing.R
@@ -0,0 +1,10 @@
+library(raster)
+library(sf)
+library(ggplot2)
+library(geosphere)
+wfolder<-"/archivio/shared/geodati/vettoriali/vaia/Cerrai_WRF_grid/"
+
+
+nodes <- readRDS("nodes.RDS")
+
+hexbin:::hexcoords(,)
